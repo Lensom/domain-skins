7e1aa285c0b4473435b8b626d38dbd36b8fe0ded
Magura <magurawork@gmail.com>
2023-10-06 09:08:14 +0300

    fix: change comment's lang

--- src/components/SkinComponents/index.js
+++ src/components/SkinComponents/index.js
@@ -3,7 +3,7 @@ import Skin1Component2 from '../../skins/skin1/Component2';
 import Skin2Component from '../../skins/skin2/Component1';
 
 const skinComponents = {
-  skin1: [Skin1Component1, Skin1Component2], // Массив компонентов для skin1
+  skin1: [Skin1Component1, Skin1Component2], // Array of components for skin1
   skin2: Skin2Component,
 };
 
